ecef5b9bd63520716917899004726729690f3729
Devin Rieger <dev0089@gmail.com>
2017-02-03 04:53:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,39 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  hash2 = {}
+  languages.map{|clas, group| group.map{|language, pair| hash2[language] = pair}}
+  languages.map{|clas, group| group.map{|language, pair| hash2[language][:style] = [clas]}}
+  hash2[:javascript][:style].unshift(:oo)
+  hash2
 end
